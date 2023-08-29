a074396c9875fe1ff81be76e146ab2feb0c9e6f4
A-d-a-m <adam3928@mail.ru>
2023-08-29 14:34:00 +0200

    added open and open_category methods

--- qa_guru_7_10/application.py
+++ qa_guru_7_10/application.py
@@ -1,8 +1,9 @@
 from qa_guru_7_10.model.components.left_panel import LeftPanel
 from qa_guru_7_10.model.pages.simple_registration_page import SimpleRegistrationPage
-from selene import browser
+from selene import browser, have
 
 
+# noinspection PyMethodMayBeStatic
 class Application:
 
     def __init__(self):
@@ -10,7 +11,10 @@ class Application:
         self.left_panel = LeftPanel()
 
     def open(self):
-        browser.open('/elements')
+        browser.open('/')
+
+    def open_category(self, value):
+        browser.all('.card-body h5').element_by(have.exact_text(value)).click()
 
 
 app = Application()
