fa60d5eb250409ad9faf1f6506fb7af9decc899d
A-d-a-m <adam3928@mail.ru>
2023-08-29 14:42:46 +0200

    added open_simple_registration_form

--- qa_guru_7_10/model/components/left_panel.py
+++ qa_guru_7_10/model/components/left_panel.py
@@ -1,6 +1,10 @@
 from selene import browser, have
 
 
+# noinspection PyMethodMayBeStatic
 class LeftPanel:
     def open(self, value):
-        browser.all('.menu-list').element_by(have.exact_text(value)).click()
+        browser.all('.menu-list span').element_by(have.exact_text(value)).click()
+
+    def open_simple_registration_form(self):
+        self.open('Text Box')
